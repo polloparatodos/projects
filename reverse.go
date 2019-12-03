d1e9faf4d2cc09216258d83187cec67372519faf
Ryan Sherer <ryan.m.sherer@gmail.com>
2019-12-03 07:17:06 +0000

    Create palindrome script in go with test

---
+++ reverse.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func Reverse(input string) bool {
+  for i := 0; i < len(input); i++ {
+    if input[i] != input[len(input)-i-1] {
+      return false
+    }
+  }
+    return true
+}
+
+func main() {
+    fmt.Println(Reverse("tacocat"))
+    fmt.Println(Reverse("tacocats"))
+}

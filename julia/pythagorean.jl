b986df3a4228768f669b70da6c4da3578b7af19f
Ryan Sherer <ryan.m.sherer@gmail.com>
2019-12-15 01:16:00 -0600

    Well that is embarrassing. I forgot to square the result (c).

---
+++ julia/pythagorean.jl
@@ -0,0 +1,7 @@
+# function to calculate the hypotenuse
+function pythagorean(a, b)
+  return a^2+b^2
+end
+
+println("Passing in a=2 and b=5. Result: ", pythagorean(2, 5))
+println("Passing in a=6 and b=2. Result: ", pythagorean(6, 2))

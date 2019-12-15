1d02f839cd97a94ca9b29201ee9babd75424aac2
Ryan Sherer <ryan.m.sherer@gmail.com>
2019-12-15 00:53:32 -0600

    Reorder vol function

--- julia/volume_of_sphere.jl
+++ julia/volume_of_sphere.jl
@@ -1,10 +1,11 @@
 # function to calculate the volume of a sphere
+# Both 'pi' and 'π' can be used, therefore we should allow the user to specify it as an arg
 function volume_of_sphere(p, r)
   if r < 0
     return "Radii can only be zero or a positive number"
+  else
+    return 4/3*p*r^3
   end
-  # Since Julia allows 'pi' and 'π', we should allow the user to specify it as an arg
-  return 4/3*p*r^3
 end
 
 println(volume_of_sphere(π,  3))

cbae648c9724299afc023da42d214082cfc1f0ad
Ryan Sherer <ryan.m.sherer@gmail.com>
2019-12-15 00:26:10 -0600

    Impliment the thing I'm testing for

--- julia/volume_of_sphere.jl
+++ julia/volume_of_sphere.jl
@@ -1,5 +1,15 @@
 # function to calculate the volume of a sphere
 function volume_of_sphere(p, r)
-    # Since Julia allows 'pi' and 'π', we should allow the user to specify it as an arg
-    return 4/3*p*r^3
+  if r < 0
+    return "Radii can only be zero or a positive number"
+  end
+  # Since Julia allows 'pi' and 'π', we should allow the user to specify it as an arg
+  return 4/3*p*r^3
 end
+
+println(volume_of_sphere(π,  3))
+println(volume_of_sphere(pi, 3))
+println(volume_of_sphere(π,  0))
+println(volume_of_sphere(pi, 0))
+println(volume_of_sphere(π,  -1))
+println(volume_of_sphere(pi, -1))

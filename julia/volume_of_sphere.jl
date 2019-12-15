c5797b568568ea1ebc64e7628887e770e2f37329
Ryan Sherer <ryan.m.sherer@gmail.com>
2019-12-14 23:57:12 -0600

    Add more tests with approx

--- julia/volume_of_sphere.jl
+++ julia/volume_of_sphere.jl
@@ -6,6 +6,27 @@ end
 
 using Test
 @testset "volume_of_sphere tests" begin
+# Perfect match:
 @test volume_of_sphere(pi, 3) == 113.09733552923254
 @test volume_of_sphere(π,  3) == 113.09733552923254
+
+# Approx match
+@test volume_of_sphere(π,  3) ≈ 113.0973355292325
+@test volume_of_sphere(pi, 3) ≈ 113.0973355292325
+@test volume_of_sphere(π,  3) ≈ 113.097335529232
+@test volume_of_sphere(pi, 3) ≈ 113.097335529232
+@test volume_of_sphere(π,  3) ≈ 113.09733552923
+@test volume_of_sphere(pi, 3) ≈ 113.09733552923
+@test volume_of_sphere(π,  3) ≈ 113.0973355292
+@test volume_of_sphere(pi, 3) ≈ 113.0973355292
+@test volume_of_sphere(π,  3) ≈ 113.097335529
+@test volume_of_sphere(pi, 3) ≈ 113.097335529
+@test volume_of_sphere(π,  3) ≈ 113.09733552
+@test volume_of_sphere(pi, 3) ≈ 113.09733552
+@test volume_of_sphere(π,  3) ≈ 113.0973355
+@test volume_of_sphere(pi, 3) ≈ 113.0973355
+
+# Lower limit of approx match
+@test volume_of_sphere(π,  3) ≈ 113.097335
+@test volume_of_sphere(pi, 3) ≈ 113.097335
 end

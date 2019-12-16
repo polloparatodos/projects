a8b3bc02200dc1c4c11fa0ef998e95b8dd8bb176
Ryan Sherer <ryan.m.sherer@gmail.com>
2019-12-15 20:42:35 -0600

    Update problem1.jl

--- julia/ProjectEuler/problem1.jl
+++ julia/ProjectEuler/problem1.jl
@@ -1,9 +1,8 @@
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-function problem1(z)
-  matches = Array[]
-  # 1:z is the range.
+function problem1(z::Int64)
+  matches = Array{Int64}[]
   for num = 1:z
     if num % 3 == 0 || num % 5 == 0
       push!(matches, [num])

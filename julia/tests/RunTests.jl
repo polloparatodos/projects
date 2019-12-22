a62a81e2a8493760e52753d591391ba5699ca429
Ryan Sherer <ryan.m.sherer@gmail.com>
2019-12-22 02:26:53 -0600

    Tidy list

--- julia/tests/RunTests.jl
+++ julia/tests/RunTests.jl
@@ -1,9 +1,4 @@
-my_tests = [
-             "concentric_circles.jl",
-             "pythagorean.jl",
-             "quadratic.jl",
-             "volume_of_sphere.jl"
-           ]
+my_tests = [ "concentric_circles.jl", "pythagorean.jl", "quadratic.jl", "volume_of_sphere.jl" ]
 
 println("Running tests:")
 for my_test in my_tests

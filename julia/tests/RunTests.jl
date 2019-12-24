44e162d8ebe238d669cabfe6aa08500639321b18
Ryan Sherer <ryan.m.sherer@gmail.com>
2019-12-23 21:35:05 -0600

    Updated RunTests.jl

--- julia/tests/RunTests.jl
+++ julia/tests/RunTests.jl
@@ -1,4 +1,10 @@
-my_tests = [ "concentric_circles.jl", "pythagorean.jl", "quadratic.jl", "volume_of_sphere.jl" ]
+my_tests = [
+             "area_of_circle.jl",
+             "concentric_circles.jl",
+             "pythagorean.jl",
+             "quadratic.jl",
+             "volume_of_sphere.jl"
+           ]
 
 println("Running tests:")
 for my_test in my_tests

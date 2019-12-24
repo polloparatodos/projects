1c2626185df87b38720cbbc1157823e49daaaad9
Ryan Sherer <ryan.m.sherer@gmail.com>
2019-12-23 21:13:57 -0600

    Updated tests/area_of_circle.jl

--- julia/tests/area_of_circle.jl
+++ julia/tests/area_of_circle.jl
@@ -2,6 +2,6 @@ using Tests
 
 @testset "area of a circle tests" begin include("../area_of_circle.jl")
 
-@test area_of_circle(pi*1^2) ≈ 3.14159265358979
+@test area_of_circle(1) ≈ 3.14159265358979
 
 end

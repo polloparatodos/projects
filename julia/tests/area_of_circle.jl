a1e1d7ca3abc4944849d11371b479b8856de8ce7
Ryan Sherer <ryan.m.sherer@gmail.com>
2019-12-24 17:54:11 -0600

    Updated tests/area_of_circle.jl

--- julia/tests/area_of_circle.jl
+++ julia/tests/area_of_circle.jl
@@ -2,8 +2,7 @@ using Test
 
 @testset "area_of_circle tests" begin include("../area_of_circle.jl")
 
-# To the 10th decimal place
-@test area_of_circle(1) ≈ 3.1415926535
+@test area_of_circle(1) == 3.141592653589793
 
 # Lower limit
 @test area_of_circle(1) ≈ 3.14159265

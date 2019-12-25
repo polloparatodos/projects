cdc382ec2f708d4ca26d2a51237b4775f5a28f5a
Ryan Sherer <ryan.m.sherer@gmail.com>
2019-12-24 18:04:31 -0600

    Updated panagram.jl

--- julia/panagram.jl
+++ julia/panagram.jl
@@ -1,5 +1,12 @@
 # Based on the example here:
 # https://exercism.io/tracks/julia/exercises/pangram/solutions/598a0248a787436c91e559728dac73b1
-function panagram(w)
+function panagram(word)
   chars = Set("a":"z")
+  iset = Set()
+  for letter in word
+    if (lc = lowercase(letter)) in chars
+      push!(iset, lc) == chars && return true
+    end
+  end
+  false
 end

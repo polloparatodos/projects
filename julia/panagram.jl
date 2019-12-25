54d8f598bb53e0a524c055fd7006513d6708bf7f
Ryan Sherer <ryan.m.sherer@gmail.com>
2019-12-24 18:07:16 -0600

    Updated panagram.jl

--- julia/panagram.jl
+++ julia/panagram.jl
@@ -1,12 +1,15 @@
-# Based on the example here:
-# https://exercism.io/tracks/julia/exercises/pangram/solutions/598a0248a787436c91e559728dac73b1
-chars = Set("a":"z")
-function panagram(word::AbstractString)
-  iset = Set()
-  for letter in word
-    if (lc = lowercase(letter)) in chars
-      push!(iset, lc) == chars && return true
+lowerchars = Set('a':'z')
+function ispangram(input::AbstractString)
+    # this is more concise (plus fun with ∩ operator), but maybe inefficient for large strings?
+    #lowerchars == Set(map(lowercase,input)) ∩ lowerchars
+
+    # this is more verbose, but will return as soon as panagram is detected for any size string
+    # maybe compiler compiler magic means it doesn't matter?
+    iset = Set()
+    for c in input
+        if (lc = lowercase(c)) in lowerchars
+          push!(iset,lc) == lowerchars && return true
+        end
     end
-  end
-  false
+    false
 end

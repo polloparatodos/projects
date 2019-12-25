286091e133244133df775c7c6eae1468254d3dc1
Ryan Sherer <ryan.m.sherer@gmail.com>
2019-12-24 18:10:46 -0600

    Updated tests/pangram.jl

--- julia/tests/pangram.jl
+++ julia/tests/pangram.jl
@@ -1,8 +1,8 @@
 using Test
 
-@testset "panagram tests" begin include("../pangram.jl")
+@testset "pangram tests" begin include("../pangram.jl")
 
-@test panagram("the quick brown fox jumps over the lazy dog")  == true
-@test panagram("the quick brown fish jumps over the lazy dog") == false
+@test pangram("the quick brown fox jumps over the lazy dog")  == true
+@test pangram("the quick brown fish jumps over the lazy dog") == false
 
 end

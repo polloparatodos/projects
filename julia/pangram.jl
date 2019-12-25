95cf52045ab582b901e7ae3f189507a280c8571f
Ryan Sherer <ryan.m.sherer@gmail.com>
2019-12-24 18:11:52 -0600

    Updated pangram.jl

--- julia/pangram.jl
+++ julia/pangram.jl
@@ -2,12 +2,12 @@
 #https://exercism.io/tracks/julia/exercises/pangram/solutions/598a0248a787436c91e559728dac73b1
 lowerchars = Set('a':'z')
 function pangram(input::AbstractString)
-    lowerchars == Set(map(lowercase,input)) ∩ lowerchars
+  iset = Set()
 
-    for c in input
-        if (lc = lowercase(c)) in lowerchars
-          push!(iset,lc) == lowerchars && return true
-        end
+  for c in input
+    if (lc = lowercase(c)) in lowerchars
+      push!(iset,lc) == lowerchars && return true
     end
-    false
+  end
+  false
 end

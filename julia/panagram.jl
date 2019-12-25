e2b04d726a83651043cd0c996ea5f26ac3bfb2c6
Ryan Sherer <ryan.m.sherer@gmail.com>
2019-12-24 18:06:08 -0600

    Updated panagram.jl

--- julia/panagram.jl
+++ julia/panagram.jl
@@ -1,7 +1,7 @@
 # Based on the example here:
 # https://exercism.io/tracks/julia/exercises/pangram/solutions/598a0248a787436c91e559728dac73b1
-function panagram(word::AbstractString)
   chars = Set("a":"z")
+function panagram(word::AbstractString)
   iset = Set()
   for letter in word
     if (lc = lowercase(letter)) in chars

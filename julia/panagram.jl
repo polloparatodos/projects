6176bd5e2c4402c30b39cc07cbabac23aa69cf08
Ryan Sherer <ryan.m.sherer@gmail.com>
2019-12-24 18:06:43 -0600

    Updated panagram.jl

--- julia/panagram.jl
+++ julia/panagram.jl
@@ -1,6 +1,6 @@
 # Based on the example here:
 # https://exercism.io/tracks/julia/exercises/pangram/solutions/598a0248a787436c91e559728dac73b1
-  chars = Set("a":"z")
+chars = Set("a":"z")
 function panagram(word::AbstractString)
   iset = Set()
   for letter in word

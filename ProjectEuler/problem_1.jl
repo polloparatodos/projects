753b4fe9700f2f1d947d7a778be00977923ed026
Ryan Sherer <ryan.m.sherer@gmail.com>
2019-12-27 09:48:31 -0600

    Updated problem_1.jl

--- ProjectEuler/problem_1.jl
+++ ProjectEuler/problem_1.jl
@@ -1,6 +1,6 @@
 function problem1(z::Int)
   # Depending on the size, we may need to bump this up
-  matches = Array{Int}[]
+  matches = Array{Int}
   for num = 1:z
     if num % 3 == 0 || num % 5 == 0
       push!(matches, [num])

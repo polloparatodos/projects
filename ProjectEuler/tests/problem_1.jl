786255d4a4d620135d4cf16491b63b5e0c1e9cdc
Ryan Sherer <ryan.m.sherer@gmail.com>
2019-12-27 09:40:46 -0600

    Updated tests/problem_1.jl

--- ProjectEuler/tests/problem_1.jl
+++ ProjectEuler/tests/problem_1.jl
@@ -1,8 +1,7 @@
 using Test
 @testset "project_euler_problem_1 tests" begin include("../problem_1.jl")
 
-# I should probably get this to reduce down to just an integer
-@test problem1(9)   == [23]
-@test problem1(999) == [233168]
+@test problem1(9)   == 23
+@test problem1(999) == 233168
 
 end

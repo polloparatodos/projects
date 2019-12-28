6f8dee6e1fbc7d83acedb9f79800265f6abdf719
Ryan Sherer <ryan.m.sherer@gmail.com>
2019-12-27 20:59:11 -0600

    Updated tests/random_language.jl

--- julia/tests/random_language.jl
+++ julia/tests/random_language.jl
@@ -1,8 +1,6 @@
 using Test
 @testset "random programming language printer outer" begin include("../random_language.jl")
 
-languages = ["julia", "ruby", "python", "javascript"]
-
 @test random_language() != ""
 
 end

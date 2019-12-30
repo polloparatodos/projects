902a9ca213b8e6f9ec76081444d943248bc8982e
Ryan Sherer <ryan.m.sherer@gmail.com>
2019-12-29 20:38:54 -0600

    Updated reverse_test.go

--- golang/reverse_test.go
+++ golang/reverse_test.go
@@ -6,6 +6,6 @@ func TestReverse(t *testing.T) {
   got := Reverse("tacocat")
 
   if got != true {
-    t.Errorf("The text is not a palindrome")
+    t.Errorf("The text '%s' is not a palindrome", got)
   }
 }

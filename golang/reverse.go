71ee97be8853397b54c4f7479b3629349eebf511
Ryan Sherer <ryan.m.sherer@gmail.com>
2019-12-29 20:48:40 -0600

    Updated reverse.go

--- golang/reverse.go
+++ golang/reverse.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func Reverse(input string) bool {
+func Reverse(input string) {
   for i := 0; i < len(input); i++ {
     if input[i] != input[len(input)-i-1] {
       return "not a palindrome"

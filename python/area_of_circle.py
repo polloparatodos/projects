f286a554f7e61957654398c6f36887ed21fab63d
Ryan Sherer <ryan.m.sherer@gmail.com>
2020-01-02 12:41:37 -0600

    Do better math

--- python/area_of_circle.py
+++ python/area_of_circle.py
@@ -1,6 +1,11 @@
 import math
 def area_of_circle(r):
-    area = math.pi*r**2;
-    print(area)
+    if r <= 0:
+        print("Radius cannot be less than or equal to 0")
+    else:
+        area = math.pi*r**2
+        print(area)
 
+area_of_circle(0)
 area_of_circle(3)
+area_of_circle(-1)

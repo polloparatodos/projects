4e71ccdd1bace7895ba3c75068db0db1de06af13
Ryan Sherer <ryan.m.sherer@gmail.com>
2020-01-06 21:33:10 -0600

    Update script to not print, just return

--- julia/area_of_circle.jl
+++ julia/area_of_circle.jl
@@ -1,6 +1,6 @@
 function area_of_circle(r)
   if r <= 0
-    println("Radius cannot be less than or equal to 0")
+    return "Radius cannot be less than or equal to 0"
   end
     return pi*r^2
 end

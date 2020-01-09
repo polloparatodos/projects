bfd495575ad37ac7cafb1e807db8d00d6f834fcb
Ryan Sherer <ryan.m.sherer@gmail.com>
2020-01-09 09:09:38 -0600

    add test for 99 to be consistent with script

--- ProjectEuler/tests/problem_1.jl
+++ ProjectEuler/tests/problem_1.jl
@@ -1,7 +1,8 @@
 using Test
 @testset "project_euler_problem_1 tests" begin include("../problem_1.jl")
 
-@test problem1(9)   == 23
+@test problem1(9)  == 23
+@test problem1(99)  == 2318
 @test problem1(999) == 233168
 
 end
